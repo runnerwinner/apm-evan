3ed547a29c384f3d729cdb0cc0c6c25fcf70c74e
runnerwinner <yangzhannjut@126.com>
2026-09-10 13:34:10 +0800

    feat: http errorcode slowhttp 信息采集

--- dogapm/http.go
+++ dogapm/http.go
@@ -5,12 +5,23 @@ import (
 	"errors"
 	"net"
 	"net/http"
+	"strconv"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
 )
 
+const (
+	httpTracerName = "dogapm/http"
+)
 
 type HttpServer struct {
 	mux *http.ServeMux
 	*http.Server
+	tracer trace.Tracer
 }
 
 
@@ -22,6 +33,7 @@ func NewHttpServer(addr string) *HttpServer {
 			Addr:    addr,
 			Handler: mux,
 		},
+		tracer: otel.Tracer(httpTracerName),
 	}
 	globalClosers = append(globalClosers, server)
 	globalStarters = append(globalStarters, server)
@@ -30,11 +42,62 @@ func NewHttpServer(addr string) *HttpServer {
 
 
 func (s *HttpServer) Handle(pattern string, handler http.Handler) {
-	s.mux.Handle(pattern, handler)
+	s.mux.Handle(pattern, &traceHandler{
+		handler: handler,
+		tracer:  s.tracer,
+	})
 }
 
 func (s *HttpServer) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
-	s.mux.HandleFunc(pattern, handler)
+	s.mux.Handle(pattern, &traceHandler{
+		handler: http.HandlerFunc(handler),
+		tracer:  s.tracer,
+	})
+}
+
+
+type traceHandler struct {
+	handler http.Handler
+	tracer trace.Tracer
+}
+
+type respWriterWrapper struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *respWriterWrapper) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func (t *traceHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if t.tracer == nil {
+		t.handler.ServeHTTP(writer, request)
+		return
+	}
+	ctx := request.Context()
+	ctx = otel.GetTextMapPropagator().Extract(ctx, propagation.HeaderCarrier(request.Header))
+	ctx, span := t.tracer.Start(ctx, request.URL.Path)
+	defer span.End()
+	request = request.Clone(ctx)
+	start := time.Now()
+	respWrapper := &respWriterWrapper{ResponseWriter: writer}
+	t.handler.ServeHTTP(respWrapper, request)
+	if respWrapper.status == 0 {
+		respWrapper.status = http.StatusOK
+	}
+	end := time.Now()
+	span.SetAttributes(
+		attribute.KeyValue{
+			Key:   "http.status_code",
+			Value: attribute.StringValue(strconv.Itoa(respWrapper.status)),
+		},
+		attribute.KeyValue{
+			Key:   "http.duration",
+			Value: attribute.IntValue(int(end.Sub(start).Seconds())),
+		},
+	)
 }
 
 func (s *HttpServer) Start() error {
